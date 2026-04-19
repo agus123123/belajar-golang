6723b676f501f58b0c9dfce2e001991c5f578d86
agus123123 <aguspurwanto0525@gmail.com>
2026-04-19 09:49:06 +0700

    main.go MAPS

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
-import "fmt"
-
-var score = 90
+import (
+	"fmt"
+)
 
 func main() {
-	sayHello("agus")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"Burger": 5.99,
+		"Pizza":  8.99,
+		"Salad":  4.99,
+		"Soda":   1.99,
 	}
 
-	showScrore()
+	fmt.Println(menu)
+	fmt.Println(menu["Pizza"])
+
+	// loping
+
+	for k, v := range menu {
+		fmt.Println(k, ":", v)
+	}
+
+	//ints as key type
+
+	phonebook := map[int]string{
+		1234567890: "Alice",
+		9876543210: "Bob",
+		5555555555: "Charlie",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[1234567890])
+
+	phonebook[1234567890] = "David"
+	fmt.Println(phonebook)
+
+	phonebook[5555555555] = "Agus"
+	fmt.Println(phonebook)
 }
